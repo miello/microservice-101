5f1d5dc55f45ed73cfa3babbeff9e262f4ee23da
miello <partearth1212@gmail.com>
2023-03-19 13:38:59 +0700

    feat: day2 implement gateway service

---
+++ src/gateway/storage/util.py
@@ -0,0 +1,27 @@
+import pika, json
+from gridfs import GridFS
+from pika.spec import PERSISTENT_DELIVERY_MODE
+
+
+def upload(f, fs: GridFS, channel, access):
+    try:
+        fid = fs.put(f)
+    except Exception as err:
+        print(err)
+        return "internal server error", 500
+
+    message = {"video_fid": str(fid), "mp3_fid": None, "username": access["username"]}
+
+    try:
+        channel.basic_publish(
+            exchange="",
+            routing_key="video",
+            body=json.dumps(message),
+            # Persistent message even if pod is crashed by making queue durable
+            properties=pika.BasicProperties(delivery_mode=PERSISTENT_DELIVERY_MODE),
+        )
+    except Exception as err:
+        print(err)
+
+        fs.delete(file_id=fid)
+        return "internal server error", 500
